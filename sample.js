8fcaa6a435463c57afd8ad18de041eb1de65481f
sruthi-vempuluru <53087346+sruthi-vempuluru@users.noreply.github.com>
2019-07-19 09:41:58 -0400

    Add files via upload

---
+++ sample.js
@@ -0,0 +1,22 @@
+alert("Welcome to my page!")
+
+function bigImg(x){
+  x.style.height = "400px";
+  x.style.width = "400px";
+}
+
+function smallImg(x){
+  x.style.height = "200px";
+  x.style.width = "200px";
+}
+
+function stext(x) {
+    x.style.color = 'blue';
+}
+
+function ptext(x){
+    x.style.color = 'purple';
+}
+function htext(x) {
+    x.style.color = 'white';
+}
